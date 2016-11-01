adffa0f01e54a1685abc3c4f077332063ba5fe7c
Neandher Carlos <neandher89@gmail.com>
2016-11-01 15:34:19 -0200

    42 Directive Property Binding - Done!

--- directives/src/app/highlight.directive.ts
+++ directives/src/app/highlight.directive.ts
@@ -1,13 +1,34 @@
-import {Directive, ElementRef, Renderer} from '@angular/core';
+import {Directive, ElementRef, Renderer, HostListener, HostBinding, Input, OnInit} from '@angular/core';
 
 @Directive({
   selector: '[highlight]'
 })
 export class HighlightDirective {
 
+  @Input() defaultColor = 'white';
+  @Input('highlight') highlightColor = 'green';
+
+  private backgroundColor: string;
+
+  @HostListener('mouseenter') mouseover (){
+    this.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave') mouseleave (){
+    this.backgroundColor = this.defaultColor;
+  }
+
+  @HostBinding('style.backgroundColor') get setColor() {
+    return this.backgroundColor;
+  }
+
   constructor(private elementRef: ElementRef, private renderer: Renderer) {
     //this.elementRef.nativeElement.style.backgroundColor = 'green';
-    this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green');
+    //this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green');
+  }
+
+  ngOnInit(){
+    this.backgroundColor = this.defaultColor;
   }
 
 }
